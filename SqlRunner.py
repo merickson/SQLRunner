61b3985ae6640e90af4fb32c1a667fc496959f23
Matt Erickson <matt@spideroak.com>
2013-02-17 10:39:34 -0600

    Modified SqlRunner to do sanity checking before creating the scratch file.

--- SqlRunner.py
+++ SqlRunner.py
@@ -1,16 +1,25 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 
 import SqlCommand
 
+settings = sublime.load_settings('SQLRunner.sublime-settings')
 
 class SqlRunnerCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        our_sql = self.view.settings().get('sqlrunner-sql')
+
+        if our_sql is None:
+            sublime.status_message("Need to set SQLRunner options in project settings!")
+            return
+
+        print self.view.settings().get("SQLRunner")
+
         sels = self.view.sel()
-        original_view = self.view
         output_view = self.view.window().new_file()
         output_view.set_scratch(True)
 
-        my_psql = SqlCommand.PostgresqlCommand(host='localhost')
+        sql_cmd = SqlCommand.get()
         for sel in sels:
-            output = my_psql.run(original_view.substr(sel))
+            output = sql_cmd.run(self.view.substr(sel))
             output_view.insert(edit, 0, output)
